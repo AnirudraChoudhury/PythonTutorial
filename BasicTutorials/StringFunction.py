9534ed2f0282ade968ab77b00c97f417abbdbd6d
ANIRUDRA CHOUDHURY <anirudrac@macmetkol.com>
2020-02-18 13:28:23 +0530

    Half teached
    Co-authored-by: dipanjan20081@hotmail.com <dipanjan20081@hotmail.com>

---
+++ BasicTutorials/StringFunction.py
@@ -0,0 +1,27 @@
+# to comment any line keyboard shortcut ctrl+k+c
+
+print("Hello Work")
+
+WorkStr = "Hello Work"
+
+stringlength = len(WorkStr)
+
+print(stringlength)
+
+#checkinh is there any word = "work" in the string
+
+cond = ("Work" in WorkStr)
+
+print(cond)
+
+
+# getting index of the particular word
+
+indexofw = WorkStr.index("Work")
+
+print(indexofw)
+
+# substring using index
+
+
+WorkStr[6:] 
